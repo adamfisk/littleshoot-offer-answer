80c4c9255884cd095e728ed2623e333774853d6e
adamfisk <a@littleshoot.org>
2007-07-11 04:31:29 +0000

    changed to include offer data
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/offer-answer@766 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/answer/AnswerProcessor.java
+++ src/main/java/org/lastbamboo/common/answer/AnswerProcessor.java
@@ -15,9 +15,12 @@ public interface AnswerProcessor
     /**
      * Process the specified answer bytes.
      * 
+     * @param offer The offer, often needed in offer/answer protocols when
+     * processing the answer. 
      * @param answer The answer.
      * @return The generated {@link Socket} from the answer.
      * @throws IOException If there's an IO error processing the answer.
      */
-    Socket processAnswer(ByteBuffer answer) throws IOException;
+    Socket processAnswer(ByteBuffer offer, ByteBuffer answer) 
+        throws IOException;
     }
