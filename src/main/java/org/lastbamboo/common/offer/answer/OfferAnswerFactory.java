349bf632258e58ffdd36f3a0e3dc0742bebae0fc
adamfisk <a@littleshoot.org>
2008-10-30 23:22:35 +0000

    lots of changes to better propagate ICE connection errors
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/offer-answer@4068 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/offer/answer/OfferAnswerFactory.java
+++ src/main/java/org/lastbamboo/common/offer/answer/OfferAnswerFactory.java
@@ -1,7 +1,5 @@
 package org.lastbamboo.common.offer.answer;
 
-import java.io.IOException;
-
 import org.apache.mina.common.ByteBuffer;
 
 /**
@@ -16,8 +14,10 @@ public interface OfferAnswerFactory
      * 
      * @param listener Listener for events during the offer/answer exchange. 
      * @return A new class for creating offers and processing answers.
+     * @throws OfferAnswerConnectException If there's an error connecting the
+     * offerer.
      */
-    OfferAnswer createOfferer();
+    OfferAnswer createOfferer() throws OfferAnswerConnectException;
     
     /**
      * Creates a new class for processing offers and creating answers.
@@ -25,16 +25,19 @@ public interface OfferAnswerFactory
      * @param offer The offer.
      * @param listener Listener for events during the offer/answer exchange. 
      * @return A new class for processing offers and creating answers.
-     * @throws IOException If we could not understand the offer from the 
-     * remote host.
+     * @throws OfferAnswerConnectException If there's an error connecting the
+     * answerer.
      */
-    MediaOfferAnswer createAnswerer(ByteBuffer offer) throws IOException;
+    MediaOfferAnswer createAnswerer(ByteBuffer offer)
+        throws OfferAnswerConnectException;
 
     /**
      * Creates an offerer that establishes a media session.
      * 
      * @return The offerer.
+     * @throws OfferAnswerConnectException If there's an error connecting the
+     * offerer.
      */
-    MediaOfferAnswer createMediaOfferer();
+    MediaOfferAnswer createMediaOfferer() throws OfferAnswerConnectException;
 
     }
