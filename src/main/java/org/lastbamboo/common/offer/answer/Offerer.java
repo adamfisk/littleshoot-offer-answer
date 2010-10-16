b9a7e4b8fc660e5d76b98d2dc13e83971d862864
adamfisk <a@littleshoot.org>
2010-10-16 23:28:54 +0000

    Updates to XMPP signaling and naming tweaks
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/offer-answer@7316 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/offer/answer/Offerer.java
+++ src/main/java/org/lastbamboo/common/offer/answer/Offerer.java
@@ -1,11 +1,24 @@
 package org.lastbamboo.common.offer.answer;
 
+import java.io.IOException;
 import java.net.URI;
 
+/**
+ * Interface for classes that send offers to other peers.
+ */
 public interface Offerer 
     {
 
-    void offer(URI sipUri, byte[] offer, 
-        OfferAnswerTransactionListener transactionListener);
+    /**
+     * Send an offer to the specified URI.
+     * 
+     * @param uri The URI indicating the user/machine to send the offer to.
+     * @param offer The raw offer data itself.
+     * @param transactionListener The listener for transaction success or
+     * failure.
+     * @throws IOException If there's an IO error sending the offer.
+     */
+    void offer(URI uri, byte[] offer, 
+        OfferAnswerTransactionListener transactionListener) throws IOException;
 
     }
